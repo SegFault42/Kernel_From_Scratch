0c36deff455c3fd982ab3d45ea59c5b8bc997cd1
SegFault42 <SegFault42@protonmail.com>
2018-11-25 10:29:09 +0700

    continue refactoring

--- srcs/shell.c
+++ srcs/shell.c
@@ -50,7 +50,6 @@ static void	hexdump_cmd(char *arg1, char *arg2)
 	uint32_t size = 0;
 
 	// Check if command good formatted
-	/*if (count_nb_elem(cmd, ' ') != 3)*/
 	if (*arg1 == 0 || *arg2 == 0) {
 		kfs_putstr_ln("Usage   : hexdump addr size");
 		kfs_putstr_ln("Example : hexdump 1000 60");
@@ -62,7 +61,7 @@ static void	hexdump_cmd(char *arg1, char *arg2)
 		for (int i = 0; i < size / 16; i++) {
 			hexdump((void *)addr);
 			addr += 16;
-			kfs_sleep(1 / 2);
+			/*kfs_sleep(1);*/
 		}
 	}
 }
@@ -82,6 +81,18 @@ static void	write_cmd(const char *addr_str, const char *str)
 	}
 }
 
+void	exec_cmd(char split[3][128])
+{
+	if (!(kfs_strcmp(split[0], "hexdump"))) {
+		hexdump_cmd(split[1], split[2]);
+	} else if (!(kfs_strcmp(split[0], "write"))) {
+		write_cmd(split[1], split[2]);
+	} else if (split[0][0] != 0) {
+		kfs_putstr(split[0]);
+		kfs_putstr_ln(": Command not found");
+	}
+}
+
 void	shell(void)
 {
 	char	cmd[128] = {0};
@@ -93,16 +104,10 @@ void	shell(void)
 	// clean command (removing extra white space)
 	kfs_remove_extra_white_space(cmd);
 
-	// split for parsw command
+	// split for parse command
 	strsplit(cmd, split, ' ');
 
-	if (!(kfs_strcmp(split[0], "hexdump"))) {
-		hexdump_cmd(split[1], split[2]);
-	} else if (!(kfs_strcmp(split[0], "write"))) {
-		write_cmd(split[1], split[2]);
-	}
-	/*else if (!(kfs_strcmp(cmd, "reboot\n"))) {*/
-		/*reboot();*/
-	/*}*/
+	// execute command
+	exec_cmd(split);
 }
 
